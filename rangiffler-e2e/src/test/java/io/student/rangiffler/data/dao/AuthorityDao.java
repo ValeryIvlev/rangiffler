56ead1cceeb8ffb4f6985a15e0ba4013de1b65e6
valerijivlev <qa@>
2026-01-24 14:15:00 +0300

    убрал ненужный импорт

--- rangiffler-e2e/src/test/java/io/student/rangiffler/data/dao/AuthorityDao.java
+++ rangiffler-e2e/src/test/java/io/student/rangiffler/data/dao/AuthorityDao.java
@@ -1,10 +1,6 @@
 package io.student.rangiffler.data.dao;
 
-import io.student.rangiffler.data.entity.Authority;
 import io.student.rangiffler.data.entity.AuthorityEntity;
-
-
-import java.util.UUID;
 
 public interface AuthorityDao{
     void createAuthority(AuthorityEntity... authority);
